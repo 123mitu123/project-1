e1b4108900d57802fb50819507e5cdf5fe4fea56
123mitu123 <>
2023-01-29 13:39:04 -0800

    123

--- server.js
+++ server.js
@@ -1,16 +1,60 @@
-const http = require("http");
+const express = require("express");
 
-const port = 8081;
+//initialization
+const app = express();
+//application will now use json format for data
+app.use(express.json());
 
-http
-    .createServer((request, response) => {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("<h1>hello, this is from my server</h1>");
-        response.end();
-    })
-    .listen(port, () => {
-        console.log('Nodejs server started on port ${port}');
+const port = 8080;
+
+const toDolist = ["complte node byte", "play cricaket"];
+
+//http://localhost:8080/todos
+app.get("/todos", (req, res) => {
+    //callback
+    res.status(200).send(toDolist);
+});
+
+//http://localhost:8080/todos
+app.post("/todos", (req,res) => {
+    //callback
+    let newToDoItem = req.body.item;
+    toDolist.push(newToDoItem);
+    res.status(200).send( {
+        message: "task added successfully",
+    });
+});
+
+app.delete("/todos", (req,res) => {
+    //callback
+    const itemToDelete = req.body.item;
+
+    toDolist.find((element, index) => {
+        if (element === itemToDelete) {
+            toDolist.splice(index, 1);
+        }
     });
 
+    res.status(202).send({
+        message: `deleted item - ${req.body.item}`,
+    });
+});
 
-// http://localhost:8081
+//just some additional examples
+//app.get("/todos/create");
+//app.post("/todos/create");
+
+//put,patch//all the other methods on a particular route
+app.all("/todos", (req,res) => {
+    res.status(501).send();
+});
+
+//all the other routers
+app.all("*", (req,res) => {
+    res.status(404).send();
+});
+
+app.listen(port, () => {
+    //callback
+    console.log(`nodejs server started on port ${port}`);
+});
